1f906dbc7fa1818afac4bf752ee146e3cf68f210
Nguyễn Duy Anh <duyanh2912@gmail.com>
2016-12-24 01:57:50 +0700

    Full HD ko che

--- Day01/Exercise3ViewController.swift
+++ Day01/Exercise3ViewController.swift
@@ -9,14 +9,32 @@
 import UIKit
 
 class Exercise3ViewController: UIViewController {
+    
+    // Outlets
     @IBOutlet weak var expressionLabel: UILabel!
+    
     @IBOutlet weak var outputLabel: UILabel!
+    var outputValue: Double = 0 {
+        didSet { updateOutput() }
+    }
     
     @IBOutlet var calculatorButtons: [UIButton]!
     
+    // Types
+    enum CharacterType {
+        case numbers, operators, equal, decimal
+    }
+    
+    // Variables
+    var currentType: CharacterType = .numbers
+    var characters = ["0"]
+    var usedAC = false
+    var usedEqual = true
+    
+    // Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         for button in calculatorButtons {
             button.addTarget(self, action: #selector(touchedButton), for: .touchUpInside)
         }
@@ -27,25 +45,253 @@ class Exercise3ViewController: UIViewController {
         navigationController?.navigationBar.barStyle = .black
         navigationController?.navigationBar.tintColor = .white
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func touchedButton() {
+    func touchedButton(_ sender: AnyObject) {
+        guard let button = sender as? UIButton else { return }
+        let text = button.title(for: .normal)!
+        
+        if text == "=" {
+            tappedEqual()
+        } else {
+            usedEqual = false
+        }
+        
+        if text == "AC" {
+            tappedAC()
+        } else {
+            usedAC = false
+        }
+        
+        if text == "+/-" {
+            tappedPositiveNegative()
+        }
+        
+        if text == "." {
+            tappedDecimal()
+        }
+        
+        if isNumber(text) {
+            tappedNumber(text)
+        }
+        
+        if isOperator(text) {
+            tappedOperator(text)
+        }
+        
+        updateExpression()
+    }
+    
+    func tappedDecimal() {
+        guard currentType != .decimal else { return }
+        
+        if currentType == .numbers {
+            guard !characters.last!.contains(".") else { return }
+            
+            outputLabel.text = characters.last! + "."
+            characters[characters.count - 1] += "."
+        }
+        if currentType == .operators  {
+            characters.append("0.")
+            outputLabel.text = "0."
+        }
+        if currentType == .equal {
+            characters.removeAll()
+            characters.append("0.")
+            outputLabel.text = "0."
+        }
+        
+        currentType = .decimal
+    }
+    
+    func tappedPositiveNegative() {
+        if currentType == .equal {
+            outputValue *= -1
+            characters.removeAll()
+            characters.append(outputToString())
+            return
+        }
+        if currentType == .decimal {
+            let text = characters.last!
+            
+            if !text.hasPrefix("-") {
+                characters[characters.count - 1] = "-" + characters[characters.count - 1]
+                outputLabel.text = "-" + outputLabel.text!
+            } else {
+                characters[characters.count - 1].remove(at: text.startIndex)
+                outputLabel.text?.remove(at: text.startIndex)
+            }
+            return
+        }
+        
+        if currentType == .numbers {
+            let number = -Double(characters.last!)!
+            outputValue = number
+            characters[characters.count - 1] = outputToString()
+        }
+        if currentType == .operators {
+            let number = -Double(characters[characters.count - 2])!
+            outputValue = number
+            currentType = .numbers
+            characters.append(outputToString())
+        }
+    }
+    
+    func tappedAC() {
+        if usedAC == true {
+            characters.removeAll()
+            characters.append("0")
+            usedAC = false
+            currentType = .numbers
+        }
+        
+        outputValue = 0
+        usedAC = true
+        
+        if currentType == .numbers {
+            if characters.count > 1 {
+                characters.removeLast()
+                currentType = .operators
+            }
+        }
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tappedEqual() {
+        guard !characters.isEmpty else { return }
+        guard characters.count > 1 else { return }
+        
+        if usedEqual {
+            var array: [String] = [outputToString()]
+            array.append(characters[characters.count - 2])
+            array.append(characters[characters.count - 1])
+            characters = array
+        }
+        
+        if currentType == .operators {
+            characters.removeLast()
+        }
+        if currentType == .decimal {
+            if (characters.last?.hasSuffix("."))! {
+                let text = characters.last!
+                characters[characters.count - 1].remove(at: text.index(before: text.endIndex))
+            }
+        }
+        
+        var array = characters
+        for i in 0...(array.count - 1) {
+            if array[i] == " x " {
+                array[i] = " * "
+                continue
+            }
+            if array[i] == " ÷ " {
+                array[i] = " / "
+                array[i+1] = String(describing: Double(array[i+1])!)
+                continue
+            }
+        }
+        
+        let expression = array.joined()
+        let nse = NSExpression(format: expression)
+        outputValue = nse.expressionValue(with: nil, context: nil) as! Double
+        
+        self.currentType = .equal
+        self.usedEqual = true
     }
-    */
-
+    
+    func tappedOperator(_ text: String) {
+        if currentType == .numbers {
+            characters.append(" \(text) ")
+        }
+        if currentType == .operators {
+            characters[characters.count - 1] = " \(text) "
+        }
+        if currentType == .equal {
+            characters.removeAll()
+            characters.append(outputToString())
+            characters.append(" \(text) ")
+        }
+        if currentType == .decimal {
+            if (characters.last?.hasSuffix("."))! {
+                let text = characters.last!
+                characters[characters.count - 1].remove(at: text.index(before: text.endIndex))
+            }
+            characters.append(" \(text) ")
+        }
+        
+        currentType = .operators
+        outputValue = Double(characters[characters.count - 2])!
+    }
+    
+    func tappedNumber(_ text: String) {
+        if currentType == .numbers {
+            if characters.last! == "0" {
+                characters[characters.count - 1] = text
+            } else {
+                characters[characters.count - 1] += text
+            }
+        }
+        
+        if currentType == .operators {
+            characters.append(text)
+        }
+        
+        if currentType == .equal {
+            characters.removeAll()
+            characters.append(text)
+        }
+        
+        if currentType == .decimal {
+            characters[characters.count - 1] += text
+        } else {
+            currentType = .numbers
+        }
+        outputValue = Double(characters[characters.count - 1])!
+        
+    }
+    
+    func outputToString() -> String {
+        if outputValue == outputValue.rounded() {
+            return String.init(format: "%.f", outputValue)
+        } else {
+            return String(describing: outputValue)
+        }
+    }
+    
+    func updateOutput() {
+        outputLabel.text = outputToString()
+    }
+    
+    func updateExpression() {
+        let text = characters.joined()
+        self.expressionLabel.text = text
+    }
+    
+    func isNumber(_ input: String) -> Bool {
+        return Int(input) != nil
+    }
+    
+    func isOperator(_ input: String) -> Bool {
+        let operators = ["+", "-", "x", "÷"]
+        for sign in operators {
+            if sign == input { return true }
+        }
+        return false
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
